8f8aec4cf6e77db4d946e9d5275d42a6dff56a7f
marrgrate <marrygran28@gmail.com>
2021-06-15 14:58:59 +0300

    openning img

--- learningopencv/learningopencv.py
+++ learningopencv/learningopencv.py
@@ -1,14 +1,15 @@
 import cv2 as cv
 import sys
-from PIL import Image
+import os
 
-path = "C:\\Users\maryia.grankouskaya\source\repos\learningopencv\learningopencv\myenv\sample\data"
-cv.samples.addSamplesDataSearchPath(path)
-img = cv.imread(cv.samples.findFile("lena.jpg"))
+path = r'C:\Users\maryia.grankouskaya\Desktop\lena.png'
+print(os.path.isfile(path)) 
+img = cv.imread(path, -1)
 
 if img is None:
     sys.exit("Could not read the image.") 
-cv.imshow("Display window", img)
+cv.imshow("LENA", img)
 k = cv.waitKey(0)
 if k == ord("s"):
     cv.imwrite("lena.png", img)
+
